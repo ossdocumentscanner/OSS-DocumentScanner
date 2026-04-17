6a0bbac83a2246922d5bcef7e43f275bfe1010f9
farfromrefuge <dev@akylas.fr>
2026-04-17 09:39:31 +0200

    chore: baseurl again

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
     url: 'https://www.akylas.fr',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/OSS-DocumentScanner/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
